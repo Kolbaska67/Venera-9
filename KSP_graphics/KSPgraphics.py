458a390c0ab650c74b113bcea2b7f82ab4214d0e
VVRybin <vovarybin237@yandex.ru>
2024-12-18 22:44:52 +0300

    Update_KSP_MathModel

--- KSP_graphics/KSPgraphics.py
+++ KSP_graphics/KSPgraphics.py
@@ -8,59 +8,36 @@ with open("graphic.json") as f:
     
 SpeedX = graphics["speedX"]
 SpeedY = graphics["speedY"]
-SpeedZ = graphics["speedZ"]
-Height = graphics["height"]
+x_coords = graphics["x_coords"]
+y_coords = graphics["y_coords"]
 Time = graphics["time"]
-DragX = graphics["dragX"]
-DragY = graphics["dragY"]
-DragZ = graphics["dragZ"]
 
 # Строим график
 
 plt.figure(figsize=(15, 15))   
 
-plt.subplot(4, 2, 1)
-plt.plot(Time, Height)
+plt.subplot(2, 2, 1)
+plt.plot(Time, x_coords)
 plt.xlabel('Время, с')
-plt.ylabel('Высота, м')
+plt.ylabel('Координата по X, м')
 plt.grid(color='black') 
 
-plt.subplot(4, 2, 2)
+plt.subplot(2, 2, 2)
+plt.plot(Time, y_coords)
+plt.xlabel('Время, с')
+plt.ylabel('Координата по Y, м')
+plt.grid(color='black') 
+
+plt.subplot(2, 2, 3)
 plt.plot(Time, SpeedX)
 plt.xlabel('Время, с')
-plt.ylabel('Скорость по оси X, км/с')
+plt.ylabel('Скорость по оси X, м/с')
 plt.grid(color='black') 
 
-plt.subplot(4, 2, 3)
+plt.subplot(2, 2, 4)
 plt.plot(Time, SpeedY)
 plt.xlabel('Время, с')
-plt.ylabel('Скорость по оси Y, км/с')
+plt.ylabel('Скорость по оси Y, м/с')
 plt.grid(color='black') 
 
-plt.subplot(4, 2, 4)
-plt.plot(Time, SpeedZ)
-plt.xlabel('Время, с')
-plt.ylabel('Скорость по оси Z, км/с')
-plt.grid(color='black') 
-
-plt.subplot(4, 2, 5)
-plt.plot(Time, DragX)
-plt.xlabel('Время, с')
-plt.ylabel('Смещение по оси X, м')
-plt.grid(color='black') 
-
-plt.subplot(4, 2, 6)
-plt.plot(Time, DragY)
-plt.xlabel('Время, с')
-plt.ylabel('Смещение по оси Y, м')
-plt.grid(color='black') 
-
-plt.subplot(4, 2, 7)
-plt.plot(Time, DragZ)
-plt.xlabel('Время, с')
-plt.ylabel('Смещение по оси Z, м')
-plt.grid(color='black') 
-
-
-plt.tight_layout(pad=1.5)
 plt.show()
--- KSP_graphics/data_read.py
+++ KSP_graphics/data_read.py
@@ -1,3 +1,4 @@
+import numpy as np
 import krpc  # библиотека для подключения к KSP и сбора данных
 import json  # библиотека для работы с json форматом
 
@@ -6,14 +7,11 @@ connection = krpc.connect(name='VARKT')
 vessel = connection.space_center.active_vessel
 
 # списки с результатами
-DragX = []
-DragY = []
-DragZ = []
-Height = []
+y_coords = []
+x_coords = []
 Time = []
 SpeedX = []
 SpeedY = []
-SpeedZ = []
 
 # Получаем ориентацию корабля
 Orbit = vessel.orbit.body.reference_frame
@@ -21,54 +19,39 @@ Orbit = vessel.orbit.body.reference_frame
 reference_frame = vessel.orbit.body.reference_frame
 
 Position = vessel.position(reference_frame)
-velocity = vessel.velocity(reference_frame)
 
-# Получение начального положения корабля для вычисления перемещения
-initialPositionX = Position[0]
-initialPositionY = Position[1]
-initialPositionZ = Position[2]
+# Начальная позиция для расчета смещения
+initial_position = vessel.position(vessel.orbit.body.reference_frame)
+# Длина вектора
+initial_position_vec_length = np.linalg.norm(initial_position)
 
 # Получаем данные
 while True:
     Time.append(vessel.met) # время в c
-    Height.append(vessel.flight(Orbit).mean_altitude) # высота в м
+    y_coords.append(vessel.flight(Orbit).mean_altitude) # высота в м
     
-    velocity = vessel.velocity(reference_frame)
-
+    # Текущее положение для расчета смещения
+    current_position = vessel.position(vessel.orbit.body.reference_frame)
+    
+    # Расчет смещения
+    current_position = current_position / np.linalg.norm(current_position) * initial_position_vec_length
+    horizontal_displacement = np.linalg.norm(current_position - initial_position)
+    
+    x_coords.append(horizontal_displacement)
+    
     # скорости в проекциях 
-    vertical_speed = vessel.flight(vessel.orbit.body.reference_frame).vertical_speed / 1000 # скорость по X в км/c
-    horizontal_speed = vessel.flight(vessel.orbit.body.reference_frame).horizontal_speed / 1000 # скорость по Y в км/c
+    vertical_speed = vessel.flight(vessel.orbit.body.reference_frame).vertical_speed  # скорость по X в м/c
+    horizontal_speed = vessel.flight(vessel.orbit.body.reference_frame).horizontal_speed  # скорость по Y в м/c
     SpeedX.append(horizontal_speed)
     SpeedY.append(vertical_speed)
-    SpeedZ.append(velocity[2])
     
-    # положение в пространстве
-
-    reference_frame = vessel.orbit.body.reference_frame
-
-    Position = vessel.position(reference_frame)
-
-    currentPositionX = Position[0]
-    currentPositionY = Position[1]
-    currentPositionZ = Position[2]
-    
-    # смешение по проециям
-    DragX.append(currentPositionX - initialPositionX)
-    DragY.append(currentPositionY - initialPositionY)
-    DragZ.append(currentPositionZ - initialPositionZ)
-
     if vessel.met > 250: 
         print("Истекло 250 секунд")   
         break
     
-    if (vessel.flight(Orbit).mean_altitude >= 70_000):
-        print("Мы вышли на орбиту")
-        break
-
 print("Данные считаны")
 
-data = {"height": Height, "time": Time, "speedX": SpeedX, "speedY": SpeedY, 
-        "speedZ": SpeedZ, "dragX": DragX, "dragY": DragY, "dragZ": DragZ}
+data = {"y_coords": y_coords, "x_coords": x_coords, "time": Time, "speedX": SpeedX, "speedY": SpeedY}
 
 with open("graphic.json", "w") as f:
     json.dump(fp=f,obj=data)
