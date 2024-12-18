5d765de6db393bb2a4f72e10b05b2c53d0717452
VVRybin <vovarybin237@yandex.ru>
2024-12-18 23:52:17 +0300

    update_comare

--- Compare_graphics/compare_graphics.py
+++ Compare_graphics/compare_graphics.py
@@ -36,62 +36,70 @@ def mediana(array: list) -> float:
     if (n % 2 == 0):
         return array[n // 2]
     return (array[n // 2 + 1] + array[n // 2]) / 2
+
+
+def otn_error(abs_error, array):
+    result = []
+    for i in range(len(array)):
+        result.append(abs_error[i] / array[i])
+    return result
         
 
-stages = [
-    {"fuel_mass": 46779, 'burn_time': 254, 'Ssop': 1.9, 'Rsop': 60},
-    {"fuel_mass": 12400, 'burn_time': 92, 'Ssop': 1.3, 'Rsop': 50},
-]
+stages = [ 
+    {'massa_stage': 50_000, 'massa_fuel': 110_000, 'burn_time': 100, 'f_tract': 2_700_000}, 
+    {'massa_stage': 15_000, 'massa_fuel': 20_000, 'burn_time': 120, 'f_tract': 500_000}, 
+    {'massa_stage': 25_000, 'massa_fuel': 600, 'burn_time': 50, 'f_tract': 50_000} 
+] 
 
 e = 2.718281828459045
 pi = 3.14
-# H = sqrt(x ** 2 + y ** 2 + z ** 2 + 2 * REARTH * z + REARTH ** 2) - REARTH
-ALPHA = 75 #кг/с
-REARTH = 600000 #м
-PHI = 0.00654 #радиан
-MU = 0.028 #кг/моль
-C = 0.5 #безразмерная
-S = 13.5 #м2
-Po0 = 1.225 #кг/м3
-G = 6.674 * 1e-11 #н*м2/кг2
-#g = 9.81 #м/с2
-M = 5.2915793e22  #кг
-Vgas = 2943 #м/с
-R = 8.314 #дж/(моль*К)
-T = 290 #кельвин
-P0 = 1#атмосфера 1,013250·10 ** 5
-Psoplo = 70#атмосфера
 
-m0 = 700000 #кг
+R_KERBIN = 600000  # м радиус Кербина
+MU = 0.028  # кг/моль молярная масса воздуха 
+C = 0.5  # безразмерная коэффициент лобового сопротивления
+S = 13.5  # м2 площадь поперечного сечения
+Po0 = 1.225  # кг/м3 плотность воздуха
+G = 6.674e-11  # н*м2/кг2 гравитационная постоянная
+M_KERBIN = 5.2915793e22  #кг масса Кербина
+R = 8.314  # дж/(моль*К) универсальная газовая постояннная
+START_T = 300  # К начальная температура
+P0 = 101_325  # Па давление 
+Psoplo = 70  # атмосфера давление сопла
+g = 9.81  # м/c2 ускорение свободного падения
 
 
-def m(t):
-    return m0 - ALPHA * t
+def corner(height: float) -> float:
+    """ Угол, в зависимости от высоты
+
+    Args:
+        height (float): высота
+
+    Returns:
+        float: угол в градусах
+    """
+    if height < 80000: 
+        return 90 * (1 - height / 80000) 
+    return 0 
 
 
-def gamma(t):
-    return pi / 2 - PHI * t
+def kf(massa_fuel: float, time: float) -> float: 
+    """ коэффициент , сколько сгорело топлива
+    Args:
+        m (float): масса топлива
+        t (float): время
+
+    Returns:
+        float: коэффициент , сколько сгорело топлива
+    """
+    return massa_fuel / time 
 
 
-def beta(t):
-    return PHI * t
+massa_rocket = 210000  # кг масса ракеты
+T = 300  # температура ракеты 
 
 
-def v_vector(vx, vy, vz):
-    return vx ** 2 + vy ** 2
-
-
-def height(x, y, z):
-    return sqrt(x ** 2 + y ** 2 + z ** 2 + 2 * REARTH * z + REARTH ** 2) - REARTH
-
-
-def acceleration_of_free_fall(h):
-    return (M * G) / ((REARTH + h) ** 2)
-
-
-def system(y, t, number_stage):
-    """system
-    Вычисление необходимой функции для решения ОДУ
+def system(y: list, t: list, num_stage: int) -> list:
+    """Вычисление необходимой функции для решения ОДУ
 
     Args:
         y (list): список переменных:
@@ -105,113 +113,128 @@ def system(y, t, number_stage):
             x8 - ускорение по Y
             x9 - ускорение по Z
         t (list): последовательность точек, для которых требуется найти значение y. 
-        number_stage (int): номер ступени
+        num_stage (int): номер ступени
 
     Returns:
         list: массив, содержащий значение y для каждого желаемого времени в t, с начальным значением y0 в первой строке.
     """
-   
-    x1, x4, x2, x5, x3, x6 = y
 
-    stage = stages[number_stage]
+    Xx, speedX, Xy, speedY = y
 
-    Ssop = stage['Ssop']
-    Rsop = stage['Rsop']
+    global T
 
-    gamma0 = gamma(t)
-    beta0 = beta(t)
+    stage = stages[num_stage]
+    
+    alpha = np.radians(corner(Xy)) # угол поворота ракеты
 
-    v = v_vector(x4, x5, x6)
-    h = height(x1, x2, x3)
-    g = acceleration_of_free_fall(h)
+    massa_fuel = stage["massa_fuel"]
+    F_traction = stage['f_tract']
+    burn_time = stage['burn_time']
+    
+    k = kf(massa_fuel, burn_time)
+    
+    if T > 50:
+        T = START_T - 6 * (Xy // 1000)
+        
+    cur_massa = massa_rocket - k * t # текущая масса = масса ракеты на данном стадии - использованное топливо
+    v = speedX ** 2 + speedY ** 2
+    
+    p = P0 * e ** ((-g * Xy * MU) / (R * T)) # давление от высоты
+    po = (p * MU) / (R * T) # плотность ракеты от высоты
+    
+    F_grav = (G * M_KERBIN * cur_massa) / ((R_KERBIN + Xy) ** 2) # сила гравитации
+    F_resist = C * S * v * po / 2 # сила сопротивления
+    
+    dSpeedX = (F_traction - F_resist) * np.cos(alpha) / cur_massa
+    dSpeedY = ((F_traction - F_resist) * np.sin(alpha) - F_grav) / cur_massa 
+    
+    return [speedX, dSpeedX, speedY, dSpeedY]
 
-    ftag = ALPHA * Vgas + Ssop * (Psoplo - P0 * e ** ((-MU * g * h) / (R * T)))
-    ftazh = (m0 - ALPHA * t) * g
-    fsop = 0.5 * v ** 2 * C * S * Po0 * e ** ((-MU * g * h) / (R * T))
 
-    summa_sqrt = x4 ** 2 + x5 ** 2 + x6 ** 2
+start_value = [0, 0, 0, 0] # начальные значения
 
-    x7 = (1 / m(t)) * np.cos(gamma0) * np.cos(beta0) * (ftag - fsop)
-    x8 = np.sin(gamma0) * np.sin(beta0) * (ftag - fsop) * (1 / m(t))
-    x9 = (-ftazh + (np.sin(gamma0)) * (-fsop + ftag) * (1 / m(t))) * (-1)
+# первая ступень 
+time1 = np.linspace(0, stages[0]["burn_time"]) # Время работы первой ступени 
+result1 = odeint(system, start_value, time1, args=(0,)) # Решение системы для первой ступени 
+ 
+# вторая ступень 
+massa_rocket -= (stages[0]['massa_stage'] + stages[0]['massa_fuel']) # Масса ракеты после отсоединения первой ступени 
+time2 = np.linspace(0, stages[1]["burn_time"], 100) # Время работы второй ступени 
+result2 = odeint(system, result1[-1, :], time2, args=(1,)) # Решение системы для второй ступени
 
 
-    return [x4, x7, x5, x8, x6, x9]
-
-
-initial_conditions = [1, 1, 1, 1, 1, 1]
-
-time1 = np.linspace(0, stages[0]["burn_time"])
-
-result1= odeint(system, initial_conditions, time1, args=(0,))
+# третья ступень 
+massa_rocket -= (stages[1]['massa_stage'] + stages[1]['massa_fuel']) # Масса ракеты после отсоединения второй ступени 
+time3 = np.linspace(0, stages[2]["burn_time"], 100) # Время работы третьей ступени 
+result3 = odeint(system, result2[-1, :], time3, args=(2,)) # Решение системы для третьей ступени 
+  
+time = np.concatenate([time1, time1[-1] + time2, time1[-1] + time2[-1] + time3]) 
 
 graphics = dict()
-with open("graphic.json") as f:
+with open("KSP_graphics/graphic.json") as f:
     graphics = json.load(f)
     
 Time = graphics["time"]
-m = min(len(Time), len(time1))
+m = min(len(Time), len(time))
 Time = Time[:m]
 time1 = time1[:m]
 
-Height = graphics["height"][:m]
-DragX = graphics["dragX"][:m]
-DragY = graphics["dragY"][:m]
-DragZ = graphics["dragZ"][:m]
+x_coords_KSP = graphics["x_coords"][:m]
+y_coords_KSP = graphics["y_coords"][:m]
+speedX_KSP = graphics["speedX"][:m]
+speedY_KSP = graphics["speedY"][:m]
 
-x1 = [result1[:, 0]][:m]
-x4 = [result1[:, 1]][:m]
-x2 = [result1[:, 2]][:m]
-x5 = [result1[:, 3]][:m]
-x3 = [result1[:, 4]][:m]
-x6 = [result1[:, 5]][:m]
+# Объединение результатов 
+Xx = np.concatenate([result1[:, 0], result2[:, 0], result3[:, 0]])[:m]
+SpeedX = np.concatenate([result1[:, 1], result2[:, 1], result3[:, 1]])[:m] 
+Xy = np.concatenate([result1[:, 2], result2[:, 2], result3[:, 2]])[:m]
+SpeedY = np.concatenate([result1[:, 3], result2[:, 3], result3[:, 3]])[:m]
 
-Abs_error_height = abs_error(Height, x3)
-Abs_error_dragX = abs_error(DragX, x1)
-Abs_error_dragY = abs_error(DragY, x2)
-Abs_error_dragZ = abs_error(DragZ, x3)
+Abs_error_y_coords = abs_error(y_coords_KSP, Xy)
+Abs_error_x_coords = abs_error(x_coords_KSP, Xx)
+Abs_error_speedX = abs_error(speedX_KSP, SpeedX)
+Abs_error_speedY = abs_error(speedY_KSP, SpeedY)
 
-
-print(f"Медина погрешности высоты: {mediana(Abs_error_height)}")
-print(f"Медина погрешности смещения по X: {mediana(Abs_error_dragX)}")
-print(f"Медина погрешности смещения по Y: {mediana(Abs_error_dragY)}")
-print(f"Медина погрешности смещения по Z: {mediana(Abs_error_dragZ)}")
+print(f"Медина погрешности координаты по Y: {mediana(Abs_error_y_coords)}")
+print(f"Медина погрешности координаты по X: {mediana(Abs_error_x_coords)}")
+print(f"Медина погрешности скорости по X: {mediana(Abs_error_speedX)}")
+print(f"Медина погрешности скорости по Y: {mediana(Abs_error_speedY)}")
 
 plt.figure(figsize=(15, 15))    
 
 plt.subplot(3, 2, 1)
-plt.plot(time1, x3, color="red", label="Высота от времени KSP")
-plt.plot(Time, Height, color="blue", label="Высота от времени МатМодель")
-plt.plot(Time, Abs_error_height, color="orange", label="Абсолютная погрешность")
+plt.plot(time1, x_coords_KSP, color="red", label="Координата по X KSP")
+plt.plot(Time, Xx, color="blue", label="Координата по X МатМодель")
+plt.plot(Time, Abs_error_x_coords, color="orange", label="Абсолютная погрешность")
 plt.xlabel('Время, с')
-plt.ylabel('Высота, м')
+plt.ylabel('Координата по X, м')
 plt.grid(color='black') 
 plt.legend()
 
-plt.subplot(3, 2, 6)
-plt.plot(time1, x1, color="red", label="Смещение по X KSP")
-plt.plot(Time, DragX, color="blue", label="Смещение по X МатМодель")
-plt.plot(Time, Abs_error_dragX, color="orange", label="Абсолютная погрешность")
+plt.subplot(3, 2, 2)
+plt.plot(time1, y_coords_KSP, color="red", label="Координата по Y KSP")
+plt.plot(Time, Xy, color="blue", label="Координата по Y МатМодель")
+plt.plot(Time, Abs_error_y_coords, color="orange", label="Абсолютная погрешность")
 plt.xlabel('Время, с')
-plt.ylabel('Смещение по оси X, м')
+plt.ylabel('Координата по Y, м')
 plt.grid(color='black') 
 plt.legend()
 
-plt.subplot(3, 2, 7)
-plt.plot(time1, x2, color="red", label="Смещение по Y KSP")
-plt.plot(Time, DragY, color="blue", label="Смещение по Y МатМодель")
-plt.plot(Time, Abs_error_dragY, color="orange", label="Абсолютная погрешность")
+plt.subplot(3, 2, 3)
+plt.plot(time1, speedX_KSP, color="red", label="Скорость по X KSP")
+plt.plot(Time, SpeedX, color="blue", label="Скорось по X МатМодель")
+plt.plot(Time, Abs_error_speedX, color="orange", label="Абсолютная погрешность")
 plt.xlabel('Время, с')
-plt.ylabel('Смещение по оси Y, м')
+plt.ylabel('Скорость по оси X, м/c')
 plt.grid(color='black') 
 plt.legend()
 
-plt.subplot(3, 2, 8)
-plt.plot(time1, x3, color="red", label="Смещение по Z KSP")
-plt.plot(Time, DragZ, color="blue", label="Смещение по Z МатМодель")
-plt.plot(Time, Abs_error_dragZ, color="orange", label="Абсолютная погрешность")
+plt.subplot(3, 2, 4)
+plt.plot(time1, speedY_KSP, color="red", label="Скорость по Y KSP")
+plt.plot(Time, SpeedY, color="blue", label="Скорость по Y МатМодель")
+plt.plot(Time, Abs_error_speedY, color="orange", label="Абсолютная погрешность")
 plt.xlabel('Время, с')
-plt.ylabel('Смещение по оси Z, м')
+plt.ylabel('Скорость по оси Y, м/c')
 plt.grid(color='black') 
 plt.legend()
 
